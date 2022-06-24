789f073e87d3d1a3b99e748500c91c610a94f11e
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2022-06-24 13:45:05 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -15,14 +15,3 @@ console.log("Category:", categories[1].textContent);
 console.log("Elements:", listEl[1]);
 console.log("Category:", categories[2].textContent);
 console.log("Elements:", listEl[2]);
-
-const categoriesEl = document.querySelector("#categories");
-console.log(`Number of categories: ${categoriesEl.children.length}`);
-
-const itemsAllEl = document.querySelectorAll("#categories .item");
-itemsAllEl.forEach((el) => {
-  const categoryEl = el.querySelector("h2");
-  const numberOfCategories = el.querySelector("ul").children.length;
-  console.log(`Category: ${categoryEl.textContent}`);
-  console.log(`Elements: ${numberOfCategories}`);
-});
